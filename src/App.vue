0a119d6e6ae9c334cc23abc3e8ccf22782608bb9
kellychen123 <kelly.chen@rentpackage.com>
2025-08-20 18:09:52 +0800

    project set up

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  mounted() {
+    console.log("API KEY:", process.env.VUE_APP_WEATHER_KEY);
+  }
+};
+</script>
